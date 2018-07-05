7c8b9725a37b00ca22f2a4e3b1cf22e172fe4674
MacStudent <macstudent@20X-iMacXX-M-406.local>
2018-07-05 16:28:43 -0400

    from akshay

--- app/src/main/java/com/example/macstudent/employeepayrollsystem/HomeScreen.java
+++ app/src/main/java/com/example/macstudent/employeepayrollsystem/HomeScreen.java
@@ -10,6 +10,6 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-        Intent home = new home
+        Intent home = new home()
     }
 }
