9874538f0e20cfac8cc9acb52f126fff51c0de41
MacStudent <macstudent@20X-iMacXX-M-394.local>
2018-07-05 17:22:48 -0400

    Login Screen

--- app/src/main/java/com/example/macstudent/employeepayrollsystem/Login.java
+++ app/src/main/java/com/example/macstudent/employeepayrollsystem/Login.java
@@ -1,14 +1,12 @@
 package com.example.macstudent.employeepayrollsystem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Login extends AppCompatActivity {
     Button login;
@@ -18,40 +16,39 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         mail=(EditText)findViewById(R.id.email);
         password=(EditText)findViewById(R.id.password);
         login=(Button)findViewById(R.id.login);
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String validemail = "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
-
-                        "\\@" +
-
-                        "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
-
-                        "(" +
-
-                        "\\." +
-
-                        "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
-
-                        ")+";
-
-String email=mail.getText().toString();
-                Matcher matcher = Pattern.compile(validemail).matcher(email);
-                if(matcher.matches()){
-                    Toast.makeText(getApplicationContext(), "Login Successfulzsdfgdfgbx",Toast.LENGTH_LONG).show();
-                }
-
-                else{
-                    Toast.makeText(getApplicationContext(), "enter valid email",Toast.LENGTH_LONG).show();
-                }
-                if (password.getText().toString().equals("")) {
-                    password.setError("Enter password");
-                }
+                Login();
             }
+
         });
 
+    private void Login()
+        {
+            String name= mail.getText().toString().trim();
+            String pass=password.getText().toString().trim();
+            if(name.equals("user@employee.com") && pass.equals("s3cr3t"))
+            {
+                Toast.makeText(this, "Credentials matched",Toast.LENGTH_LONG).show();
+                Intent home = new Intent(Login.this,HomeScreen.class);
+                startActivity(home);
+            }
+            else
+                Toast.makeText(this, "Error Login",Toast.LENGTH_LONG).show();
+
+        }
     }
+
 }
+
+
+
+
+
+
